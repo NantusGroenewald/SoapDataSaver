904e727b9b064d15d777d44b57eda3abc24571f5
Nantus Groenewald <nantusg@agilebridge.co.za>
2024-08-26 20:28:53 +0200

    Basic Project Structure

---
+++ SoapToDatabase/DataAccess/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(60)]
+        public string Surname { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string PasswordHash { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLoginDate { get; set; }
+        public bool IsActive { get; set; } = true;
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}
---
+++ SoapToDatabase/DataAccess/Models/UserRole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class UserRole
+    {
+        [Key]
+        public int UserRoleId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
